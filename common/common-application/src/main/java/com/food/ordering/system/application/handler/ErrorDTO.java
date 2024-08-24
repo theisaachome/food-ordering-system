0dc1090ab5ea0d3e5e496a21b85e49f4b7629fbf
theisaachome <65864160+theisaachome@users.noreply.github.com>
2024-08-24 23:15:42 +0800

    Implemented ControllerAdvice for Global Error Handling.

---
+++ common/common-application/src/main/java/com/food/ordering/system/application/handler/ErrorDTO.java
@@ -0,0 +1,14 @@
+package com.food.ordering.system.application.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ErrorDTO {
+    private final String code;
+    private final String message;
+
+}
